9c39839ba01bdf515cf30ee275930ee16e33db6c
Anibal Alfaro <aalfarob@sodimac.cl>
2022-09-11 17:12:17 -0300

    add style page delete js

--- src/pages/Eliminar.js
+++ src/pages/Eliminar.js
@@ -3,12 +3,29 @@ import '../css/Consulta.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import Cookies from 'universal-cookie';
+import Logo from '../../src/static/Logo.jpg';
+import AccountMenu from '../components/AccountMenu.js';
+import {
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    Button,
+    Grid,
+    TextField,
+    Typography,
+    DialogContentText,
+  } from '@mui/material';
 
 const baseUrl="http://localhost:3002/documentos";
 const cookies = new Cookies();
 
-class Eliminar extends Component {
-    state={
+const Eliminar = () => {
+
+    const [dialogValidation,setDialogValidation] = React.useState(false);
+    const [dialogDeleteConfirmation,setDialogDeleteConfirmation] = React.useState(false);
+
+    const state={
         form:{
             tipodoc: '',
             fechainicial: '',
@@ -16,17 +33,17 @@ class Eliminar extends Component {
         }
     }
 
-    handleChange=async e=>{
+    const handleChange = async (e) =>{
         this.setState({
             form:{
-                ...this.state.form,
+                ...state.form,
                 [e.target.name]: e.target.value
             }
         });
     }
 
-    eliminarDoc=async()=>{
-        await axios.get(baseUrl, {params: {tipodoc: this.state.form.tipodoc, fechainicial: this.state.form.fechainicial, fechafinal: this.state.form.fechafinal}})
+    const eliminarDoc = async()=>{
+        await axios.get(baseUrl, {params: {tipodoc: state.form.tipodoc, fechainicial: state.form.fechainicial, fechafinal: state.form.fechafinal}})
         .then(response=>{
             return response.data;
         })
@@ -36,64 +53,187 @@ class Eliminar extends Component {
                 cookies.set('tipodoc', respuesta.tipodoc, {path: "/"});
                 cookies.set('fechainicial', respuesta.sucursal, {path: "/"});
                 cookies.set('fechafinal', respuesta.correlativo, {path: "/"});
-                alert(`Encontrado ${respuesta.tipodoc} ${respuesta.sucursal} ${respuesta.correlativo}`);
-                window.location.href="./menu";
+                setDialogDeleteConfirmation(true);
+                
             }else{
-                alert('Documento NO Encontrado');
+                setDialogValidation(true)
             }
         })
         .catch(error=>{
             console.log(error);
+            setDialogValidation(true)
         })
 
     }
-
-    componentDidMount() {
-        if(cookies.get('tipodoc')){
-            window.location.href="./menu";
-        }
-    }
     
-
-    render() {
         return (
-    <div className="containerPrincipal">
-        ELIMINA DOCUMENTOS
-        <div className="containerSecundario">
-          <div className="form-group">
-            <label>Tipo Documento: </label>
-            <br />
-            <input
-              type="text"
-              className="form-control"
-              name="tipodoc"
-              onChange={this.handleChange}
-            />
-            <br />
-            <label>FechaInicial: </label>
-            <br />
-            <input
-              type="fechainicial"
-              className="form-control"
-              name="fechainicial"
-              onChange={this.handleChange}
-            />
-            <br />
-            <label>FechaFinal: </label>
-            <br />
-            <input
-              type="fechafinal"
-              className="form-control"
-              name="fechafinal"
-              onChange={this.handleChange}
-            />
-            <br />
-            <button className="btn btn-primary" onClick={()=> this.eliminarDoc()}>Eliminar Docto</button>
-          </div>
-        </div>
-      </div>
+      <>
+            <Grid
+            container
+            style={{ background: "linear-gradient(#FFFFFF 30%, #003CFF)" }}
+            textAlign="center"
+            rowSpacing={5}
+            gap={1}
+            id="PageGrid"
+          >
+             <Grid item xs={12}>
+
+            </Grid>
+            <Grid item xs={1}>
+
+            </Grid>
+            <Grid item xs={2} textAlign="left" >
+            <img
+                src={Logo}
+                width={50} height={50}
+                style={{borderRadius: '50%'}}
+                />
+            </Grid>
+            <Grid item xs={6}>
+            
+            </Grid>
+            <Grid item xs={2} textAlign="right">
+            <AccountMenu/>
+            </Grid>
+            <Grid item xs={1}>
+            
+            </Grid>
+            <Grid item xs={12}>
+
+            </Grid>
+            <Grid item xs={12}>
+
+            </Grid>
+            <Grid item xs={5}>
+
+            </Grid>
+            <Grid container textAlign="Center" rowSpacing={1} xs={2}>
+
+
+
+                <Grid item xs={12}>
+                <TextField id="outlined-basic" label="Tipo documento" variant="outlined"
+                type="text"
+                className="form-control"
+                name="tipodoc"
+                onChange={handleChange}
+                />
+                </Grid>
+
+                <Grid item xs={12}>
+                <TextField id="outlined-basic" label="Fecha inicial" variant="outlined"
+                type="text"
+                className="form-control"
+                name="fechainicial"
+                onChange={handleChange}
+                />
+                </Grid>
+
+                <Grid item xs={12}>
+                <TextField id="outlined-basic" label="Fecha final" variant="outlined"
+                type="text"
+                className="form-control"
+                name="fechafinal"
+                onChange={handleChange}
+                />
+                </Grid>
+               
+                <Grid item xs={12}>
+                <Button variant="contained"  onClick={()=> 
+                {
+                    
+                    eliminarDoc();
+                    
+                }} style={{maxWidth: '250px', maxHeight: '40px', minWidth: '250px', minHeight: '40px'}}> Elimina  documentos </Button>
+                </Grid>
+                <Grid item xs={12}>
+                
+                </Grid>
+                
+           
+            </Grid>
+            <Grid item xs={5}>
+
+            </Grid>
+            <Grid item xs={12}>
+
+            </Grid>
+            <Grid item xs={12}>
+
+            </Grid>
+            <Grid item xs={12}>
+
+            </Grid>
+            <Grid item xs={12}>
+
+            </Grid>
+            <Grid item xs={12}>
+
+            </Grid>
+            <Grid item xs={12}>
+
+            </Grid>
+            <Grid item xs={12}>
+
+            </Grid>
+            <Grid item xs={12}>
+
+            </Grid>
+            <Grid item xs={12}>
+
+            </Grid>
+
+            </Grid>
+
+    <Dialog
+        open={dialogValidation}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Ups!, algo salió mal..."}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Al parecer los datos suministrados no coinciden, favor intentar nuevamente.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setDialogValidation(false)}>
+            Aceptar
+          </Button>
+        </DialogActions>
+    </Dialog>
+
+
+    <Dialog
+        open={dialogDeleteConfirmation}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Documento encontrado"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Presione boton aceptar para confirmar.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => 
+            {
+            setDialogDeleteConfirmation(false);
+            window.location.href="./menu";
+            }
+            }>
+            Aceptar
+          </Button>
+        </DialogActions>
+      </Dialog>
+            </>
+
         );
-    }
+    
 }
 
 export default Eliminar;
